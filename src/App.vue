226afb37973fc88dbfb78ca6dedd20ffa7fba8bb
Chris <59448319+rorocc@users.noreply.github.com>
2024-10-27 13:23:15 +0100

    model setup

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import IipSimulator from "@/components/iip-simulator.vue";
+</script>
+
+<template>
+  <header />
+
+  <main class="container m-auto">
+    <iip-simulator />
+  </main>
+
+  <footer />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/iip-simulator.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <h1 class="text-2xl bold my-4">Integrated Information Processing Model</h1>
+    <iip-model />
+  </div>
+</template>
+
+<script>
+import IipModel from "@/components/iip-model.vue";
+export default {
+  name: "iip-simulator",
+  components: {IipModel}
+}
+</script>
+
+<style scoped>
+
+</style>
